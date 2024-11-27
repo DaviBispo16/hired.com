80603eac7b872d98588c2bb71d1388d07d022086
Davi Bispo <davibispo@localhost.localdomain>
2024-11-27 18:32:26 -0300

    fix: changing the name of the job search variable

--- backend/src/controllers/jobs-controllers.ts
+++ backend/src/controllers/jobs-controllers.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from "express";
+import {v4 as uuidv4} from 'uuid';
 import Jobs from '../../models/Jobs';
 
-export const getJobs = async (req: Request, res: Response) => {
+export const getAllJobs = async (req: Request, res: Response) => {
     try {
-        const get = await Jobs.findAll();
-        res.status(200).json(get);
+        const allJobs = await Jobs.findAll();
+        res.status(200).json(allJobs);
     }  catch (error:any) {
         res.status(500).json({message: `${error.message}`});
     }
 }
+
+
+
+
+
+
 
 
