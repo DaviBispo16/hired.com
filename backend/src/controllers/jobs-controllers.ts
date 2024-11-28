06d242e0e4d8bf529379d5add6d6123816e8e6ff
Davi Bispo <davibispo@localhost.localdomain>
2024-11-27 21:27:25 -0300

    fix: changing error return in res and changing jobById status code

--- backend/src/controllers/jobs-controllers.ts
+++ backend/src/controllers/jobs-controllers.ts
@@ -1,15 +1,13 @@
 import { Request, Response } from "express";
 import {v4 as uuidv4} from 'uuid';
 import Jobs from '../../models/Jobs';
-import { where } from "sequelize";
-
 
 export const getAllJobs = async (req: Request, res: Response) => {
     try {
         const allJobs = await Jobs.findAll();
         res.status(200).json(allJobs);
     }  catch (error:any) {
-        res.status(500).json({message: `${error.message}`});
+        res.status(500).json({messagem: 'server error'});
     }
 };
 
@@ -25,8 +23,8 @@ export const registerJob = async (req: Request, res: Response) => {
             empresa
         });
         res.status(201).json(createJob);
-    } catch (error: any) {
-        res.status(500).json({message: `${error.message}`});
+    } catch (error) {
+        res.status(500).json({messagem: 'server error'});
     }
 };
 
@@ -35,8 +33,8 @@ export const getJobById = async (req: Request, res: Response) => {
     try {
         const jobById = await Jobs.findByPk(id);
         res.status(200).json(jobById);
-    } catch (error: any) {
-        res.status(500).json({message: `${error.message}`});
+    } catch (error) {
+        res.status(500).json({messagem: 'server error'});
     }
 };
 
@@ -46,7 +44,7 @@ export const updateJob = async (req: Request, res: Response) => {
     try {
         const jobById = await Jobs.findByPk(id);
         if (jobById == null) {
-            res.status(401).json({message: "ID not found"});
+            res.status(404).json({message: "ID not found"});
             return
         }
         const changeJob = await Jobs.update({
@@ -58,25 +56,25 @@ export const updateJob = async (req: Request, res: Response) => {
             where: {id}
         });
         res.status(201).json(jobById);
-    } catch (error: any) {
-        res.status(500).json({message: `${error.message}`});
+    } catch (error) {
+        res.status(500).json({messagem: 'server error'});
     }
-}
+};
 
 export const deleteJobById = async (req: Request, res: Response) => {
     const {id} = req.params;
     try {
         const jobById = await Jobs.findByPk(id);
         if (jobById == null) {
-            res.status(401).json({mensagem: 'ID not found'});
-            return;
+            res.status(404).json({mensagem: 'ID not found'});
+            return
         }
         const removeJob = await Jobs.destroy({where : {id}});
         res.status(204).json();
-    } catch (error: any) {
-        res.status(500).json({message:  `${error.message}`});
+    } catch (error) {
+        res.status(500).json({messagem: 'server error'});
     }
-}
+};
 
 
 
