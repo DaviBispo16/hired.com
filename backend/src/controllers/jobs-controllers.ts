ff6462d79b75cbe277118a5d95b6b31c2e65ca9d
Davi Bispo <davibispo@localhost.localdomain>
2024-11-27 21:00:46 -0300

    feat: created functionality update job and the router put

--- backend/src/controllers/jobs-controllers.ts
+++ backend/src/controllers/jobs-controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import {v4 as uuidv4} from 'uuid';
 import Jobs from '../../models/Jobs';
 
+
 export const getAllJobs = async (req: Request, res: Response) => {
     try {
         const allJobs = await Jobs.findAll();
@@ -37,6 +38,29 @@ export const getJobById = async (req: Request, res: Response) => {
         res.status(500).json({message: `${error.message}`});
     }
 };
+
+export const updateJob = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const {descricao, titulo, telefone, empresa} = req.body;
+    try {
+        const jobById = await Jobs.findByPk(id);
+        if (jobById == null) {
+            res.status(401).json({message: "ID not found"});
+            return
+        }
+        const changeJob = await Jobs.update({
+            descricao,
+            titulo,
+            telefone, 
+            empresa
+        }, {
+            where: {id}
+        });
+        res.status(201).json(jobById);
+    } catch (error: any) {
+        res.status(500).json({message: `${error.message}`});
+    }
+}
 
 
 
--- backend/src/routes/jobs-routes.ts
+++ backend/src/routes/jobs-routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAllJobs, registerJob, getJobById} from '../controllers/jobs-controllers';
+import { getAllJobs, registerJob, getJobById, updateJob} from '../controllers/jobs-controllers';
 
 const router = express.Router();
 
 router.get('/vagas', getAllJobs);
 router.post('/vagas', registerJob);
 router.get('/vagas/:id', getJobById);
+router.put('/vagas/:id', updateJob);
 
 export default router;
