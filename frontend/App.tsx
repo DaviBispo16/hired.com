c59f3f90a8bc6d9851ef3bdd04a943161103452a
Davi Bispo <davibispo@localhost.localdomain>
2024-11-28 22:06:41 -0300

    feat: added react navigation for the registration and login screens

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,18 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScrren";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <LoginScreen/>
+   <NavigationContainer>
+      <Stack.Navigator initialRouteName='LoginScreen' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='LoginScreen' component={LoginScreen}/>
+        <Stack.Screen name='RegisterScreen' component={RegisterScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- frontend/src/components/AreaNavegation/index.tsx
+++ frontend/src/components/AreaNavegation/index.tsx
@@ -1,10 +1,20 @@
 import { Container, Heading, ButtomNavigation, NavigationButtonTitle } from "./styles";
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+    LoginScreen: undefined;
+    RegisterScreen: undefined;
+  };
+
+type NavigationProps = NativeStackNavigationProp<RootStackParamList>;
 
 export function AreaNavegationLogin() {
+    const navigation = useNavigation<NavigationProps>();
     return (
         <Container>
             <Heading>Não possui uma conta?</Heading>
-                <ButtomNavigation>
+                <ButtomNavigation onPress={() => navigation.navigate('RegisterScreen')}>
                     <NavigationButtonTitle>Criar</NavigationButtonTitle>
                 </ButtomNavigation>
         </Container>
@@ -12,12 +22,13 @@ export function AreaNavegationLogin() {
 }
 
 export function AreaNavegationRegister() {
+    const navigation = useNavigation<NavigationProps>();
     return (
         <Container>
             <Heading>Já possui conta?</Heading>
-                <ButtomNavigation>
+                <ButtomNavigation onPress={() => navigation.navigate('LoginScreen')}>
                     <NavigationButtonTitle>Acessar</NavigationButtonTitle>
                 </ButtomNavigation>
         </Container>
-    )
+    );
 }
