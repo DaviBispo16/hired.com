28bf806a5ef18a1e0cba9e44bcc6207bb6050910
Davi Bispo <davibispo@localhost.localdomain>
2024-11-29 22:49:17 -0300

    feat: adding a connection to the jobs api using the axios lib

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from "./src/screens/LoginScreen";
-import RegisterScreen from "./src/screens/RegisterScrren";
+import RegisterScreen from "./src/screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
--- frontend/src/screens/LoginScreen/index.tsx
+++ frontend/src/screens/LoginScreen/index.tsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+import { Alert } from "react-native";
 import { Container, Login, Heading, Subtitles, InputButton, EntryButtonTitle, RedirectArea} from "./styles";
 import { InputAddEmail, InputAddPasscode } from "../../components/InputAdd";
 import { AreaNavegationLogin } from "../../components/AreaNavegation";
+import axios from "axios";
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleLogin = async () => {
+        if (!email || !password) {
+            Alert.alert('Error', 'Please fill in all fields');
+            return;
+        }
+        try {
+            const response = await axios.post('http://10.0.2.2:3000/usuario/login', {
+                email,
+                senha: password
+            });
+            console.log(response.data);
+        } catch (error: any) {
+            alert(error.request ? 'Não foi possível criar a conta' : 'Erro de rede: Nenhuma resposta recebida do servidor.');
+        }
+    } 
     return (
         <Container>
             <Login>
                 <Heading>Acesse sua conta</Heading>
-                    <Subtitles>Email</Subtitles>
-                    <InputAddEmail/>
+                    <Subtitles>Email</Subtitles>                    
+                    <InputAddEmail email={email} onChangeEmail={setEmail}/>
                     <Subtitles>Senha</Subtitles>
-                    <InputAddPasscode/>
-                    <InputButton>
+                    <InputAddPasscode passcode={password} onChangePasscode={setPassword}/>
+                    <InputButton onPress={handleLogin}>
                         <EntryButtonTitle>Entrar</EntryButtonTitle>
                     </InputButton>
                     <RedirectArea>
